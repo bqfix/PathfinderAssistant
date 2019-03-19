f6e1aa9f78e4d70307c1c9187882d93137cdd019
bqfix <benjaminfix@gmail.com>
2019-03-19 12:53:51 -0400

    Added strings
    To create a deck of cards for later use

--- app/src/main/java/com/example/android/pathfinderassistant/CardListActivity.kt
+++ app/src/main/java/com/example/android/pathfinderassistant/CardListActivity.kt
@@ -8,5 +8,20 @@ class CardListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_list)
+
+        //Initialize a deck of cards
+        val deck = listOf(Card(getString(R.string.dazzle_name), getString(R.string.dazzle_descrip)),
+            Card(getString(R.string.frigid_blast_name), getString(R.string.frigid_blast_descrip)),
+            Card(getString(R.string.fiery_glare_name), getString(R.string.fiery_glare_descrip)),
+            Card(getString(R.string.lightning_touch_name), getString(R.string.lightning_touch_descrip)),
+            Card(getString(R.string.sagacity_name), getString(R.string.sagacity_descrip)),
+            Card(getString(R.string.magic_padded_armor_name), getString(R.string.magic_padded_armor_descrip)),
+            Card(getString(R.string.sages_journal_name), getString(R.string.sages_journal_descrip)),
+            Card(getString(R.string.wand_of_paralyze_name), getString(R.string.wand_of_paralyze_descrip)),
+            Card(getString(R.string.frog_name), getString(R.string.frog_descrip)),
+            Card(getString(R.string.teamster_name), getString(R.string.teamster_descrip)),
+            Card(getString(R.string.apprentice_name), getString(R.string.apprentice_descrip)),
+            Card(getString(R.string.blessing_of_ascension_name), getString(R.string.blessing_of_ascension_descrip)),
+            Card(getString(R.string.blessing_of_shax_name), getString(R.string.blessing_of_shax_descrip)))
     }
 }
