f8b28a5a54859a8292530334098518ecffdfb929
bqfix <benjaminfix@gmail.com>
2019-03-19 13:02:35 -0400

    Created Activities and Fragments

---
+++ app/src/main/java/com/example/android/pathfinderassistant/CardDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.pathfinderassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CardDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_detail)
+    }
+}
---
+++ app/src/main/java/com/example/android/pathfinderassistant/CardRecyclerAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.android.pathfinderassistant
+
+class CardRecyclerAdapter {
+}
