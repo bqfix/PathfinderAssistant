1dd58f5c4ba718d50403f6ffa0c69f093fc131a8
bqfix <benjaminfix@gmail.com>
2019-03-19 16:09:53 -0400

    Added Recycler Logic

--- app/src/main/java/com/example/android/pathfinderassistant/CardRecyclerAdapter.kt
+++ app/src/main/java/com/example/android/pathfinderassistant/CardRecyclerAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.android.pathfinderassistant
 
-class CardRecyclerAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.card_recycler_item.view.*
+
+class CardRecyclerAdapter internal constructor(private val cardClickHandler : CardClickHandler) : RecyclerView.Adapter<CardRecyclerAdapter.CardViewHolder>() {
+
+    private var mCards : List<Card>? = null
+
+    //Inner ViewHolder Class
+    inner class CardViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val mName : TextView
+
+        init {
+            mName = itemView.cardrecycler_name
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val card = mCards!![adapterPosition]
+            cardClickHandler.onItemClick(card)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CardViewHolder {
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val view = inflater.inflate(R.layout.card_recycler_item, viewGroup, false)
+
+        return CardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(cardViewHolder: CardViewHolder, position: Int) {
+        val currentCard = mCards!![position]
+        cardViewHolder.mName.setText(currentCard.name)
+    }
+
+    override fun getItemCount(): Int {
+        return if (mCards == null) 0 else mCards!!.size
+    }
+
+    interface CardClickHandler {
+        fun onItemClick(card:Card)
+    }
 }
